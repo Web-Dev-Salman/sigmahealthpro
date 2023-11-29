e8d2c3e434b62e9f9a7efb72fa94f9767294f2c0
Web-Dev-Salman <webdevsalmansharif@gmail.com>
2023-11-29 20:27:07 +0600

    fix sidebar header

--- src/Layout/Sidebar/Sidebar.jsx
+++ src/Layout/Sidebar/Sidebar.jsx
@@ -4,15 +4,15 @@ import logoSigma from '../../assets/Logo/SigmaHealthProLogo.svg';
 import { Link } from 'react-router-dom';
 import { CgMenu } from "react-icons/cg";
 import DashboardIcon from "../../assets/Icon/Control Panel.png"
-import EnrollmentReqiestIcon from "../../assets/Icon/Invite.svg"
-import PatientManagementIcon from "../../assets/Icon/Management.svg"
-import VaccineManagementIcon from "../../assets/Icon/Vaccine Management.svg"
-import SiteManagementIcon from "../../assets/Icon/Product Management.svg"
-import UserManagementIcon from "../../assets/Icon/Businessman.svg"
-import AccessManagementIcon from "../../assets/Icon/Conference.svg"
-import ReferenceDataIcon from "../../assets/Icon/Admin Settings Male.svg"
-import VaccineForecastingIcon from "../../assets/Icon/Vaccine Management.svg"
-import ReportsIcon from "../../assets/Icon/Graph Report.svg"
+import EnrollmentReqiestIcon from "../../assets/Icon/Invite.png"
+import PatientManagementIcon from "../../assets/Icon/Management.png"
+import VaccineManagementIcon from "../../assets/Icon/Vaccine Management.png"
+import SiteManagementIcon from "../../assets/Icon/Product Management.png"
+import UserManagementIcon from "../../assets/Icon/Businessman.png"
+import AccessManagementIcon from "../../assets/Icon/Conference.png"
+import ReferenceDataIcon from "../../assets/Icon/Admin Settings Male.png"
+import VaccineForecastingIcon from "../../assets/Icon/Vaccine Management.png"
+import ReportsIcon from "../../assets/Icon/Graph Report.png"
 
 const Sidebar = ({ onMenuClick }) => {
 
@@ -88,25 +88,25 @@ const Sidebar = ({ onMenuClick }) => {
     const [collapsed, setCollapsed] = useState(true);
     return (
         // w-64
-        <div className={` z-10 h-screen relative `}>
+        <div className={` z-10  relative `}>
             <div className='logo-div bg-white-900 p-4 '>
-                <CgMenu className={` text-seconderyColor text-3xl rounded absolute -right-28 top-[25px] cursor-pointer1`} onClick={() => setCollapsed(!collapsed)} />
                 <div className='inline-flex'>
-                    <img className={`w-full pl-4 ${!collapsed && "rotate-[360deg] w-full"} duration-500`} src={logoIconSigma} alt="" />
-                    <img className={`w-auto duration-300 absolute top-[30px] left-[65px] ${!collapsed && "d-hidden "}`} src={logoSigma} alt="" />
+                    <img className={`w-full pl-4 duration-500`} src={logoIconSigma} alt="" />
+                    <img className={`w-auto duration-300 absolute top-[30px] left-[65px]`} src={logoSigma} alt="" />
                 </div>
 
             </div>
             {/* w-[115px] */}
-            <div className={`w-[115px] pt-5 bg-bgColor h-screen flex flex-col items-center`}>
+            <div className={`w-[115px] pt-5 bg-bgColor h-[calc(100vh-82px)] flex flex-col items-center`}>
                 <ul>
                     {navlinks.map((menu, index) => (
                         <Link onClick={() => onMenuClick(menu.name)} to={menu.link} className={`group rounded-md flex flex-col items-center text-center align-middle gap-2`}>
                             <li key={index} />
                             <img className=" w-8 iconColor text-center text-[#727272]" src={menu.icon} />
-                            <span className={`pl-1 text-sideBarTextColor`}>{menu.name}</span>
-                            {/* {!collapsed && <div className={`absolute rounded-md px-2 py-1 w-max bg-mainColor text-white left-full invisible group-hover:visible`}>{menu.name}</div>} */}
-                        </Link>
+                            <span className={`pl-1 text-sideBarTextColor leading-4	`}>{menu.name}</span>
+                            <div onClick={() => onMenuClick()}>
+                            </div>
+                        </Link> 
                     ))}
                 </ul>
             </div>
